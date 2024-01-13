f1691e429ea49bf4cf302085dfad60ad3ae40e55
KensProjects <kdwilbusiness@gmail.com>
2024-01-13 14:46:53 -0500

    fix quantity error  src/app/_components/catalog/CatalogComp.tsx 	add quantity  src/app/catalogData.json

--- src/app/_components/catalog/CatalogComp.tsx
+++ src/app/_components/catalog/CatalogComp.tsx
@@ -23,7 +23,7 @@ export default function CatalogComp() {
         <ul className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 justify-center items-center gap-4 p-4 w-full h-full">
             {catalog.map(({ name, price, imageSrc }: Catalog) => (
                 <li key={name} className="flex justify-center items-center h-60">
-                    <CatalogItem name={name} price={price} imageSrc={imageSrc} />
+                    <CatalogItem name={name} price={price} imageSrc={imageSrc} quantity={0}/>
                 </li>
             ))}
         </ul>
